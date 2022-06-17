08c6c520c61392605f3ef7a287a6aa2f81eb0763
Mackenzie Marriott <Mackenziejmarriott@gmail.com>
2022-06-17 10:42:55 +0100

    changes to MP3Py
    fixed threading issue
    added thread for media playing
    added play, pause, volup and voldown

--- MP3Py.py
+++ MP3Py.py
@@ -1,31 +1,52 @@
 #functionality modules
 import vlc
 import time
+import threading
 
 #gui modules
 import tkinter
 from tkinter import *
+from tkinter import ttk #use for table to display songs
 
-#template music player function
-def template():
+#thread creation template
+class mediaThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
 
     vlcInstance = vlc.Instance()
-
+    #media player instance
     player = vlcInstance.media_player_new()
+    player.audio_set_volume(50)
 
-    media = vlcInstance.media_new("01 Kalimba.mp3")
+    #function to play media
+    def mediaPlayerPlay(self, fileName):
+        media = self.vlcInstance.media_new(fileName)
+        self.player.set_media(media)
+        
+        self.player.play()
+        time.sleep(0.5)
+    
+    def mediaPlayerPause(self):
+        self.player.pause()
+    
+    def mediaPlayerVolUp(self):
+        currentVolume = self.player.audio_get_volume()
+        if currentVolume < 100:
+            self.player.audio_set_volume(currentVolume+10)
+    
+    def mediaPlayerVolDown(self):
+        currentVolume = self.player.audio_get_volume()
+        if currentVolume > 0:
+            self.player.audio_set_volume(currentVolume-10)
 
-    player.set_media(media)
+    def run(self):
+        print ("Starting " + self.name)
 
-    player.play()
 
-    time.sleep(0.5)
 
-    duration = player.get_length()
-
-    print(duration)
-
-    time.sleep(duration)
 
 def __main__():
     def GUI():
@@ -44,7 +65,11 @@ def __main__():
         mediaControlsBG.place(relx=0.5, rely=0.6, anchor=N)
 
         #functionality test
-        playButton = tkinter.Button(mediaControlsBG, text="PLAY", command=template).place(relx=0.5, rely=0.5, anchor=CENTER)
+        musicThread = mediaThread(1, "Music thread", 1)
+        playButton = tkinter.Button(mediaControlsBG, text="PLAY", command=lambda:[musicThread.mediaPlayerPlay("01 Kalimba.mp3")]).place(relx=0.5, rely=0.5, anchor=CENTER)
+        pauseButton = tkinter.Button(mediaControlsBG, text="PAUSE", command=musicThread.mediaPlayerPause).place(relx=0.5, rely=0.6, anchor=CENTER)
+        volUp = tkinter.Button(mediaControlsBG, text="VOLUME UP", command=musicThread.mediaPlayerVolUp).place(relx=0.7, rely=0.55, anchor=CENTER)
+        volDown = tkinter.Button(mediaControlsBG, text="VOLUME DOWN", command=musicThread.mediaPlayerVolDown).place(relx=0.3, rely=0.55, anchor=CENTER)
 
         screen.mainloop()
     
